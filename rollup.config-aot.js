8971bc45f17c888b11fa590fe0380a76ecf3dbbc
Jorge Torrente <jorge.torrente@t-systems.com>
2017-01-18 12:42:52 +0100

    new rollup warnings filter

--- rollup.config-aot.js
+++ rollup.config-aot.js
@@ -13,20 +13,29 @@ export default {
         nodeResolve({ jsnext: true, module: true }),
 
         commonjs({
-            include: [ 'node_modules/rxjs/**', 
-            'node_modules/primeng/**', 'node_modules/log4javascript/**', 'node_modules/lodash/**',
-            'node_modules/crypto-js/**' ],
-            
+            include: ['node_modules/rxjs/**',
+                'node_modules/primeng/**', 'node_modules/log4javascript/**', 'node_modules/lodash/**',
+                'node_modules/crypto-js/**'],
+
             namedExports: {
-                'node_modules/primeng/primeng.js': [ 'MessagesModule', 'GrowlModule', 'PanelModule', 'DropdownModule',
-                'ToggleButtonModule', 'BreadcrumbModule', 'ConfirmDialogModule', 'ConfirmationService' ],
-                'node_modules/log4javascript/log4javascript.js': [ 'AjaxAppender', 'AlertAppender', 'BrowserConsoleAppender',
-                'NullLayout', 'SimpleLayout', 'PatternLayout', 'XmlLayout', 'JsonLayout', 'HttpPostDataLayout',
-                'setEnabled', 'getLogger', 'getRootLogger', 'Level', 'Logger' ],
-                'node_modules/lodash/lodash.js': [ 'find' ],
-                'node_modules/crypto-js/index.js': [ 'AES', 'enc' ]
+                'node_modules/primeng/primeng.js': ['MessagesModule', 'GrowlModule', 'PanelModule', 'DropdownModule',
+                    'ToggleButtonModule', 'BreadcrumbModule', 'ConfirmDialogModule', 'ConfirmationService'],
+                'node_modules/log4javascript/log4javascript.js': ['AjaxAppender', 'AlertAppender', 'BrowserConsoleAppender',
+                    'NullLayout', 'SimpleLayout', 'PatternLayout', 'XmlLayout', 'JsonLayout', 'HttpPostDataLayout',
+                    'setEnabled', 'getLogger', 'getRootLogger', 'Level', 'Logger'],
+                'node_modules/lodash/lodash.js': ['find'],
+                'node_modules/crypto-js/index.js': ['AES', 'enc']
             }
         }),
         uglify()
-    ]
+    ],
+    onwarn: function (warn) {
+        if (/THIS_IS_UNDEFINED/.test(warn.code)) return;
+
+        console.error('-----------');
+        console.error(warn.message);
+        if (!/MISSING_EXPORT/.test(warn.code)){
+            console.error(warn.loc);
+        }
+    }
 }
